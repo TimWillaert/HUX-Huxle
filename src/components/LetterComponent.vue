80ca64c31dee010e27a4410bab3612db55185092
Brenden Grace <nodachi13@gmail.com>
2022-12-31 18:18:19 -0700

    implemented word rows and keyboard input

---
+++ src/components/LetterComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tile">
+    {{ letter }}
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  letter: {
+    type: String,
+    default: " ",
+  }
+});
+
+</script>
+
+<style scoped>
+.tile {
+  width: 70px;
+  height: 70px;
+  font-size: 2rem;
+  line-height: 2rem;
+  font-weight: bold;
+  text-align: center;
+  justify-content: center;
+  text-transform: uppercase;
+  user-select: none;
+  position: relative;
+  background-color: white;
+  color: black;
+  border: 3px solid gray;
+}
+</style>
---
+++ src/components/RowComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <LetterComponent v-for="i in 5" :key="i" :letter="value[i - 1]"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LetterComponent from './LetterComponent.vue';
+
+const props = defineProps({
+  value: {
+    type: String,
+    default: "",
+  },
+  solution: String,
+  submitted: Boolean,
+});
+
+</script>
+
+<style scoped>
+.row {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 5px;
+}
+</style>
