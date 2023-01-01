0eda6130992dcfc11dd00b4a53b5c57fd8d49171
Brenden Grace <nodachi13@gmail.com>
2022-12-31 18:18:09 -0700

    implemented word rows and keyboard input

--- src/components/PlayHuxle.vue
+++ src/components/PlayHuxle.vue
@@ -1,10 +1,17 @@
 <template>
-  <p>Encoded string: {{ id }}</p>
+  <!-- <p>Encoded string: {{ id }}</p>
   <p>Decoded string: {{ decoded }}</p>
   <p>English word: {{ wordEnglish }}</p>
-  <p>German word: {{ wordGerman }}</p>
-  <div class ="flex flex-col h-screen max-w-md mx-auto justify-evenly">
-    <KeyboardComponent  @key="onKey"/>
+  <p>German word: {{ wordGerman }}</p> -->
+  <div class="flex flex-col h-screen max-w-md mx-auto justify-evenly">
+    <RowComponent
+      v-for="(guess, i) in state.guesses"
+      :key="i"
+      :value="guess"
+      :solution="state.solution"
+      :submitted="i < state.currentGuess"
+    />
+    <KeyboardComponent @key="onKey" />
   </div>
 </template>
 
@@ -12,7 +19,8 @@
 import { ref } from "vue";
 import { useRoute } from "vue-router";
 import KeyboardComponent from './KeyboardComponent.vue';
-import { onUnmounted } from 'vue'
+import { onUnmounted, reactive } from 'vue'
+import RowComponent from "./RowComponent.vue";
 
 const route = useRoute();
 
@@ -22,11 +30,16 @@ const decoded = window.atob(id);
 const wordEnglish = ref(decoded.split("#")[1]);
 const wordGerman = ref(decoded.split("#")[2]);
 
+const state = reactive({
+  guesses: ["", "", "", "", "", ""],
+  currentGuess: 0,
+  solution: wordEnglish,
+})
+
+
 //Todo: validate if the decoded string is valid (should start with huxle, should have 2 hashtags, total length should be 17 characters)
 //If valid: start game
 //If invalid: show error pop-up (example in CreateHuxle.vue)
-
-let allowInput = true
 
 const onKeyup = (e: KeyboardEvent) => onKey(e.key)
 
@@ -37,16 +50,37 @@ onUnmounted(() => {
 })
 
 function onKey(key: string) {
-  if (!allowInput) return;
-  console.log(key);
-  // if (/^[a-zA-Z]$/.test(key)) {
-  //   fillTile(key.toLowerCase())
-  // } else if (key === 'Backspace') {
-  //   clearTile()
-  // } else if (key === 'Enter') {
-  //   completeRow()
-  // }
+  if (state.currentGuess >= 6) return;
+  const guess = state.guesses[state.currentGuess];
+  if (key === 'Enter')
+  {
+    if (guess.length == 5)
+    {
+      state.currentGuess++;
+    }
+  }
+  else if (key === 'Backspace')
+  {
+    state.guesses[state.currentGuess] = guess.slice(0, -1);
+  }
+  else if (guess.length < 5 && /^[a-zA-Z]$/.test(key))
+  {
+    state.guesses[state.currentGuess] += key;
+  }
+  console.log(state.guesses[state.currentGuess]);
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+#board {
+  display: grid;
+  grid-template-rows: repeat(6, 1fr);
+  grid-gap: 5px;
+  padding: 10px;
+  box-sizing: border-box;
+  --height: min(420px, calc(var(--vh, 100vh) - 310px));
+  height: var(--height);
+  width: min(350px, calc(var(--height) / 6 * 5));
+  margin: 0px auto;
+}
+</style>
